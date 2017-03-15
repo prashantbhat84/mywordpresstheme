70d72257887fcb41894e50bae57e6874f1a2bf34
Prashant Bhat <prashantbhat84@gmail.com>
2017-03-15 14:26:19 +0530

    'first'

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+if(have_posts()):
+  while(have_posts()): the_post();  ?>
+<article class="post page">
+  <h2> <?php the_title(); ?></h2>
+  <p>  <?php the_content(); ?></p>
+</article>
+
+
+<?php endwhile;
+else:
+  echo "<p> No Content Found</p>";
+endif;
+get_footer(); ?>
